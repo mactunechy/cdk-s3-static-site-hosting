3ec765fd2bc52105d8d4b7aeb2a36cf221032311
Dellan Muchengapadare <mactunechy@gmail.com>
2024-03-18 13:56:16 +0200

    add cdk infra

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,87 @@
+import {
+  Stack,
+  StackProps,
+  SecretValue,
+  aws_codepipeline as codepipeline,
+  aws_codepipeline_actions as codepipelineActions,
+  aws_codebuild as codebuild,
+  aws_s3 as s3,
+} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class InfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const sourceOutput = new codepipeline.Artifact()
+    const buildOutput = new codepipeline.Artifact()
+
+    const sourceAction = new codepipelineActions.GitHubSourceAction({
+      actionName: 'GithubSource',
+      owner: 'mactunechy',
+      repo: 'cdk-s3-static-site-hosting',
+      oauthToken: SecretValue.secretsManager('github_token2'),
+      output: sourceOutput,
+      branch: 'master'
+    })
+
+    const buildAction = new codepipelineActions.CodeBuildAction({
+      actionName: 'Build',
+      project: new codebuild.PipelineProject(this, 'ViteSiteBuildProject', {
+        buildSpec: codebuild.BuildSpec.fromObject({
+          version: '0.2',
+          phases: {
+            install: {
+              commands: [
+                'cd app',
+                'npm install pnpm', 
+                'pnpm install'
+              ],
+            },
+            build: {
+              commands: [ 'pnpm run build' ],
+            },
+          },
+          artifacts: {
+            files: ['**/*'],
+            'base-directory': 'app/dist',
+          },
+
+        })
+      }),
+      input: sourceOutput,
+      outputs: [buildOutput],
+    })
+
+    const bucket = new s3.Bucket(this, 'ViteStiteBucket', {
+      websiteIndexDocument: 'index.html'
+    })
+
+    const deployAction = new codepipelineActions.S3DeployAction({
+      actionName: 'S3Deploy',
+      input: buildOutput,
+      bucket
+    })
+
+    const pipeline = new codepipeline.Pipeline(this, 'ViteSitePipeline', {
+      pipelineName: 'ViteSitePipeline',
+      // let's save CMK since we're not doing any cross-account deployments
+      crossAccountKeys: false,
+    });
+
+    pipeline.addStage({
+      stageName: 'Source',
+      actions:[sourceAction],
+    })
+
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [buildAction]
+    })
+
+    pipeline.addStage({
+      stageName: 'Deploy',
+      actions: [deployAction]
+    })
+  }
+}
